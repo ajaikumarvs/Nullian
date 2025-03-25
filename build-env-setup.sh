9705465cef13cbd091e2c468b4dcd3a2132dbbc9
ajaikumarvs <ajaikumarvs@gmail.com>
2025-03-25 09:15:00 +0530

    updated build env setup to include debian-archive-keyring

--- build-env-setup.sh
+++ build-env-setup.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install live-build debootstrap xorriso isolinux syslinux-common
+sudo apt-get install live-build debootstrap xorriso isolinux syslinux-common debian-archive-keyring
